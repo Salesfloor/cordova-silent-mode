c84ddf7d34376b1c5403d60ef992a79f863decc4
Veaceslav Botnaru <veaceslav@salesfloor.net>
2023-05-26 15:17:05 -0400

    FC-581 Error method should pass a parameter.

--- src/android/SilentMode.java
+++ src/android/SilentMode.java
@@ -25,7 +25,7 @@ public class SilentMode extends CordovaPlugin {
         callbackContext.success();
         break;
       default:
-        callbackContext.error();
+        callbackContext.error("");
         break;
     }
   }
